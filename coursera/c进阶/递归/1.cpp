b8ac4f2e15ad94309895f9feea6f02a154a53adc
chenyang.fang <529765905@qq.com>
2022-03-14 17:32:22 +0800

    test

--- coursera/c进阶/递归/1.cpp
+++ coursera/c进阶/递归/1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<iostream>//单词翻转（使用递归并通过空格分隔实现分段翻转）
 using namespace std;
 char words[500];int i = 0;
 
@@ -6,19 +6,19 @@ int reverse(){
     char m = words[i]; i++;
     if (m == ' ')
     {
-        return 1;
+        return 1;  
     }
     if (m != ' '&& m != '\0')
     {
         reverse();
         cout<<m;
-        return 1;
+        return 1;    //必须返回不然每次少个空格
     }
 }
 
 int main(){
     cin.getline(words,500);   
-    while (words[i] != '\0') {
+    while (words[i] != '\0') {       //使用while do 控制多个返回值情况
 		if (reverse() == 1) {
 			cout << ' ';
 		}
