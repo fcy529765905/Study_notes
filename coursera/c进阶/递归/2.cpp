145a77269f2233b2fabceb5ab9c6475f7e175280
chenyang.fang <529765905@qq.com>
2022-03-15 18:20:53 +0800

    1

---
+++ coursera/c进阶/递归/2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std ;//角谷猜想 
+
+int cal(int n){
+    if (n == 1)
+    {
+        cout<<"End"<<endl;
+    }
+    else if (n %2 == 0)
+    {
+        cout<<n<<"/2="<<n/2<<endl;
+        cal(n/2);
+    }
+    else
+    {
+        cout<<n<<"*3+1="<<n*3+1<<endl;
+        cal(n*3+1);
+    }
+    return 0;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cal(n);
+    return 0;
+}
